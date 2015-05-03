4974b833a7708a16beff619257292ceefc549503
snakech <zihao.chen31@gmail.com>
2015-05-03 22:36:38 +1200

    pushed missed file

---
+++ iKitchen.Web/Models/ContactUsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace iKitchen.Web.Models
+{
+    public class ContactUsViewModel
+    {
+        [Required(ErrorMessage = "{0}can not be empty")]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "{0}can not be empty")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0}can not be empty")]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+    }
+}
