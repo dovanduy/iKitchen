a21b8b766d481b945dea5c92d9d19d3c24d12a5a
snakech <zihao.chen31@gmail.com>
2015-05-07 23:14:19 +1200

    IK-27 As a visitor, I want to contact iKitchen team, so that I can give them feedback directly
    changed input to html input box which can have input validation now

--- iKitchen.Web/Models/ContactUsViewModel.cs
+++ iKitchen.Web/Models/ContactUsViewModel.cs
@@ -5,15 +5,17 @@ namespace iKitchen.Web.Models
 {
     public class ContactUsViewModel
     {
-        [Required(ErrorMessage = "{0}can not be empty")]
-        [Display(Name = "UserName")]
+        [Required(ErrorMessage = "{0} can not be empty")]
+        [Display(Name = "Name")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "{0}can not be empty")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Email address can not be empty")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "{0}can not be empty")]
+        [Required(ErrorMessage = "{0} can not be empty")]
         [Display(Name = "Message")]
         public string Message { get; set; }
     }
